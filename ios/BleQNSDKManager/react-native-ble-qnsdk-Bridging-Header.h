71cf6e2b44b4c517b8b995903bf8e0c29076e69e
Jeffrey Drakos <jeffreydrakos@Jeffreys-MacBook-Pro.local>
2020-04-14 15:36:01 -0400

    BleQNSDKManager

--- ios/BleQNSDKManager/react-native-ble-qnsdk-Bridging-Header.h
+++ ios/BleQNSDKManager/react-native-ble-qnsdk-Bridging-Header.h
@@ -2,5 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import "QNDeviceSDK.h"
+#import "QNConfig.h"
+#import "QNBleConnectionChangeProtocol.h"
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
