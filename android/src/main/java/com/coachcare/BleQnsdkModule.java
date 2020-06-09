7796285ff5981e8871fccf1f1c39720760fd097d
Jeffrey Drakos <captainjeff@Jeffreys-MacBook-Pro.local>
2020-06-09 14:51:07 -0400

    moving some comments

--- android/src/main/java/com/coachcare/BleQnsdkModule.java
+++ android/src/main/java/com/coachcare/BleQnsdkModule.java
@@ -29,7 +29,6 @@ import com.coachcare.User;
 import com.qingniu.qnble.utils.QNLogUtils;
 import com.yolanda.health.qnblesdk.constant.QNUnit;
 import com.yolanda.health.qnblesdk.listener.QNBleConnectionChangeListener;
-//import com.yolanda.health.qnblesdk.listener.QNDataListener;
 import com.yolanda.health.qnblesdk.listener.QNScaleDataListener;
 import com.yolanda.health.qnblesdk.listener.QNResultCallback;
 import com.yolanda.health.qnblesdk.out.QNBleApi;
@@ -70,16 +69,13 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
   public BleQnsdkModule(ReactApplicationContext reactContext) {
 
     super(reactContext);
-    Log.w("Jeff", "on init");
 
     reactContext.addLifecycleEventListener(this);
     this.reactContext = reactContext;
-//        this.initialize();
   }
 
   public void initialize() {
 
-//    QNLogUtils.setLogEnable(BuildConfig.DEBUG);
     if (this.loaded == false) {
       final ReactApplicationContext context = getReactApplicationContext();
       mQNBleApi = QNBleApi.getInstance(context);
@@ -92,8 +88,6 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
     }
 
     this.loaded = true;
-    Log.w("Jeff", "on initialize");
-    Log.w("Jeff", "on initialize loaded" + this.loaded);
   }
 
   public void setConfig() {
@@ -101,23 +95,8 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
     mQnConfig.setNotCheckGPS(true);
     mQnConfig.setAllowDuplicates(false);
     mQnConfig.setDuration(0);
-//    mQnConfig.setScanOutTime(mConfig.getScanOutTime());
-//    mQnConfig.setConnectOutTime(mConfig.getConnectOutTime());
-//    mQnConfig.setUnit(mConfig.getUnit());
     mQnConfig.setOnlyScreenOn(false);
-    Log.w("Jeff", "on setNotCheckGPS");
-//    mQnConfig.setOnlyScreenOn(false);
-//    mQnConfig.setScanOutTime(mConfig.getScanOutTime());
-//    mQnConfig.setConnectOutTime(mConfig.getConnectOutTime());
-//    mQnConfig.setUnit(mConfig.getUnit());
-//    mQnConfig.setOnlyScreenOn(mConfig.isOnlyScreenOn());
-    //设置扫描对象
-//    mQnConfig.save(new QNResultCallback() {
-//      @Override
-//      public void onResult(int i, String s) {
-//        Log.d("ScanActivity", "initData:" + s);
-//      }
-//    });
+
     mQnConfig.save(new QNResultCallback() {
       @Override
       public void onResult(int i, String s) {
@@ -127,10 +106,7 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
   }
 
   public void initSDK() {
-//    System.out.print ("Jeff: on create");
-    Log.w("Jeff", "on initSDK");
     String encryptPath = "file:///android_asset/123456789.qn";
-//    QNBleApi mQNBleApi = QNBleApi.getInstance(this.reactContext);
     mQNBleApi.initSdk("123456789", encryptPath, new QNResultCallback() {
       @Override
       public void onResult(int code, String msg) {
@@ -141,19 +117,17 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
 
   @Override
   public void onHostResume() {
-    // Activity `onResume`
-    Log.w("Jeff", "on onHostResume");
     this.initialize();
   }
 
   @Override
   public void onHostPause() {
-    Log.w("Jeff", "on onHostPause");
+    Log.w("Scale", "on onHostPause");
   }
 
   @Override
   public void onHostDestroy() {
-    Log.w("Jeff", "on onHostDestroy");
+    Log.w("S", "on onHostDestroy");
   }
 
   private QNUser createQNUser() {
@@ -212,7 +186,7 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
       userShape, userGoal, mUser.getClothesWeight(), new QNResultCallback() {
         @Override
         public void onResult(int code, String msg) {
-          Log.d("ConnectActivity", "创建用户信息返回:" + msg);
+          Log.d("ConnectActivity", "Response:" + msg);
         }
       });
   }
@@ -221,19 +195,6 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
   public String getName() {
     return "QNSDKManager";
   }
-
-//    @ReactMethod
-//    public void buildUser(String name, int height, String gender, String id, int unit, String athleteType, Callback successCallback, Callback errorCallback) {
-//      System.out.print ("Jeff: buildUser");
-//        // TODO: Implement some actually useful functionality
-//     //   callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
-//        try {
-//          //  "buildUser", this.birthday, this.height, this.gender, this.id, this.unit, this.athleteType
-//          successCallback.invoke("build user success");
-//          } catch (IllegalViewOperationException e) {
-//          errorCallback.invoke("build user reject", e);
-//          }
-//    }
 
   @ReactMethod
   public void buildUser(String name, String birthday, int height, String gender, String id, int unit, int athleteType, Promise promise) {
@@ -252,7 +213,7 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
         height, gender, formattedBirthday, athleteType, new QNResultCallback() {
           @Override
           public void onResult(int code, String msg) {
-            Log.w("Jeff Build User", "Build User message:" + msg);
+            Log.w("Build User", "Build User message:" + msg);
           }
         });
 
@@ -261,101 +222,62 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
 
       promise.resolve("build user success");
     } catch (IllegalViewOperationException | ParseException e) {
-      Log.w("Jeff", "error");
       promise.reject("build user reject", e);
     }
   }
 
-//  public static String dateToString(Date date, String formatShort) {
-//    return dateToString(date, FORMAT_SHORT);
-//  }
-
   public static void verifyPermissions(Activity activity) {
-    //判断是否已经赋予权限
-
-    Log.w("Jeff", "verifyPermissionsss");
     if (ContextCompat.checkSelfPermission(activity,
       Manifest.permission.ACCESS_COARSE_LOCATION)
       != PackageManager.PERMISSION_GRANTED) {
-      Log.w("Jeff", "ACCESS_COARSE_LOCATION 1");
-      //如果应用之前请求过此权限但用户拒绝了请求，此方法将返回 true。
-
     }
     if (ActivityCompat.shouldShowRequestPermissionRationale(activity,
       Manifest.permission.ACCESS_COARSE_LOCATION)) {
-      Log.w("Jeff", "ACCESS_COARSE_LOCATION 2");
-      //这里可以写个对话框之类的项向用户解释为什么要申请权限，并在对话框的确认键后续再次申请权限
     }
 
 
     if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
       ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-      Log.w("Jeff", "IN FIRST ACCESS_FINE_LOCATION");
       return;
     } else {
-      Log.w("Jeff", "IN SECOND ACCESS_FINE_LOCATION");
-      // Write you code here if permission already given.
     }
 
     if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
       ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
-      Log.w("Jeff", "IN FIRST ACCESS_COARSE_LOCATION");
       return;
     } else {
-      Log.w("Jeff", "IN SECOND ACCESS_COARSE_LOCATION");
-      // Write you code here if permission already given.
     }
   }
 
   public void setConnectionListener() {
-    Log.w("Jeff", "setConnectionListener");
     final ReactApplicationContext context = getReactApplicationContext();
     mQNBleApi.setBleConnectionChangeListener(new QNBleConnectionChangeListener() {
-      //connecting
       @Override
       public void onConnecting(QNBleDevice device) {
-        Log.w("Jeff", "onConnecting");
-//        setBleStatus(QNScaleStatus.STATE_CONNECTING);
       }
 
-      //connected
       @Override
       public void onConnected(QNBleDevice device) {
-        Log.w("Jeff", "onConnected");
-//        setBleStatus(QNScaleStatus.STATE_CONNECTED);
       }
 
       @Override
       public void onServiceSearchComplete(QNBleDevice device) {
-        Log.w("Jeff", "onServiceSearchComplete");
-//The status is the method after the search service is completed, usually this method does not require business logic
       }
 
       //The connection is being disconnected. When the disconnection is called, it will be called back immediately.
       @Override
       public void onDisconnecting(QNBleDevice device) {
-//        setBleStatus(QNScaleStatus.STATE_DISCONNECTING);
-        Log.w("Jeff", "onDisconnecting");
       }
 
       //Disconnect, callback after disconnect
       @Override
       public void onDisconnected(QNBleDevice device) {
-//        setBleStatus(QNScaleStatus.STATE_DISCONNECTED);
-        Log.w("Jeff", "onDisconnected");
       }
 
       //There was a connection error, please refer to the attached table for the error code
       @Override
       public void onConnectError(QNBleDevice device, int errorCode) {
-//        setBleStatus(QNScaleStatus.STATE_DISCONNECTED);
-        Log.w("Jeff", "onConnectError");
       }
-
-//      @Override
-//      public void onScaleStateChange(QNBleDevice qnBleDevice, int i) {
-//        Log.w("Jeff", "onScaleStateChange");
-//      }
     });
 
   };
@@ -366,7 +288,6 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
 
   public void setDataListener() {
 
-//    mQNBleApi.setBleConnectionChangeListener(new QNBleConnectionChangeListener() {
     mQNBleApi.setDataListener(new QNScaleDataListener() {
 
       @Override
@@ -391,7 +312,7 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
       public void onGetScaleData(QNBleDevice device, QNScaleData data) {
         WritableMap params = Arguments.createMap();
         params.putString("status", "complete");
-//        data.getAllItem();
+
         QNScaleItemData bmrValue = data.getItem(QNIndicator.TYPE_BMR);
         if (bmrValue != null) {
           Double value = bmrValue.getValue() * 1000;
@@ -442,25 +363,18 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
 
       @Override
       public void onGetStoredScale(QNBleDevice device, List<QNScaleStoreData> storedDataList) {
-//This method is to receive the stored data on the scale side, the processing method of the stored data can refer to the demo, or you can define it yourself
-        Log.w("Jeff", "onGetStoredScale");
       }
 
       @Override
       public void onGetElectric(QNBleDevice device, int electric) {
-//This is the percentage of electricity obtained, only the electricity obtained by the charging scale is meaningful
-        Log.w("Jeff", "onGetElectric");
       }
 
-      //Connection status during measurement
       @Override
       public void onScaleStateChange(QNBleDevice device, int status) {
-        Log.w("Jeff", "setDataListener");
       }
 
       @Override
       public void onScaleEventChange(QNBleDevice qnBleDevice, int i) {
-        Log.w("Jeff", "onScaleEventChange");
       }
     });
   }
@@ -470,7 +384,6 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
     mQNBleApi.setBleDeviceDiscoveryListener(new QNBleDeviceDiscoveryListener() {
       @Override
       public void onDeviceDiscover(QNBleDevice device) {
-        Log.w("Jeff", "onDeviceDiscoverr");
         mQNBleApi.connectDevice(device, createQNUser(), new QNResultCallback() {
           @Override
           public void onResult(int code, String msg) {
@@ -478,30 +391,20 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
           }
         });
 
-//        devices.add(device);
-//        listAdapter.notifyDataSetChanged();
       }
 
       @Override
       public void onStartScan() {
-        Log.w("Jeff", "onStartScan");
 //        QNLogUtils.log("ScanActivity", "onStartScan");
 //        isScanning = true;
       }
 
       @Override
       public void onStopScan() {
-        Log.w("Jeff", "onStopScan");
-//        QNLogUtils.log("ScanActivity", "onStopScan");
-//        isScanning = false;
-//        ToastMaker.show(ScanActivity.this, "已经停止扫描");
-//        final ReactApplicationContext context = getReactApplicationContext();
         mQNBleApi.stopBleDeviceDiscovery(new QNResultCallback() {
           @Override
           public void onResult(int code, String msg) {
-            QNLogUtils.log("ScanActivity", "onStopScan");
             if (code == CheckStatus.OK.getCode()) {
-//              isScanning = false;
             }
           }
         });
@@ -509,21 +412,18 @@ public class BleQnsdkModule extends ReactContextBaseJavaModule implements Lifecy
 
       @Override
       public void onScanFail(int code) {
-        Log.w("Jeff", "onScanFail");
-//        isScanning = false;
-//        QNLogUtils.log("ScanActivity", "onScanFail:" + code);
-//        Toast.makeText(ScanActivity.this, "扫描异常，请重启手机蓝牙!", Toast.LENGTH_SHORT).show();
+        Log.w("Scale", "onScanFail");
       }
 
       @Override
       public void onBroadcastDeviceDiscover(QNBleBroadcastDevice qnBleBroadcastDevice) {
-        Log.w("Jeff", "onBroadcastDeviceDiscover");
+        Log.w("Scale", "onBroadcastDeviceDiscover");
 
       }
 
       @Override
       public void onKitchenDeviceDiscover(QNBleKitchenDevice qnBleKitchenDevice) {
-        Log.w("Jeff", "onKitchenDeviceDiscover");
+        Log.w("Scale", "onKitchenDeviceDiscover");
       }
     });
   }
