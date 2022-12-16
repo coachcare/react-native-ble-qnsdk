9c2dd2474fc4ce48dd1ba09e794716ba8328e59a
CaptainJeff <CaptainJeff@users.noreply.github.com>
2022-12-16 15:27:34 -0500

    adding headers

--- ios/BleQNSDKManager/react-native-ble-qnsdk-Bridging-Header.h
+++ ios/BleQNSDKManager/react-native-ble-qnsdk-Bridging-Header.h
@@ -2,6 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 #import "QNDeviceSDK.h"
+#import "libQingNiuSDK.a"
+#import "QingNiuDevice.h"
+#import "QingNiuUser.h"
+#import "QingNiuSDK.h"
 #import "QNConfig.h"
 #import "QNBleConnectionChangeProtocol.h"
 #import <React/RCTBridgeModule.h>
