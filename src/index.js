0b631a6daf61f2a568224cd7ebe2ec0d0b9740e9
CaptainJeff <CaptainJeff@users.noreply.github.com>
2023-08-18 11:54:30 -0400

    fix: testing export

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
-const LINKING_ERROR = `The package 'react-native-ble-qnsdk' doesn't seem to be linked. Make sure: \n\n` +
-    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-    '- You rebuilt the app after installing the package\n' +
-    '- You are not using Expo Go\n';
+const LINKING_ERROR =
+  `The package 'react-native-ble-qnsdk' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
 const BleQnsdk = NativeModules.BleQnsdk
-    ? NativeModules.BleQnsdk
-    : new Proxy({}, {
+  ? NativeModules.BleQnsdk
+  : new Proxy(
+      {},
+      {
         get() {
-            throw new Error(LINKING_ERROR);
+          throw new Error(LINKING_ERROR);
         },
-    });
+      }
+    );
 const QnsSDKEmitter = new NativeEventEmitter(BleQnsdk);
 function buildUser(user) {
-    return BleQnsdk.buildUser(user);
+  return BleQnsdk.buildUser(user);
 }
 function onStartDiscovery() {
-    return BleQnsdk.onStartDiscovery();
+  return BleQnsdk.onStartDiscovery();
 }
 function onStopDiscovery() {
-    return BleQnsdk.onStopDiscovery();
+  return BleQnsdk.onStopDiscovery();
 }
-export { BleQnsdk, QnsSDKEmitter, buildUser, onStartDiscovery, onStopDiscovery };
+export {
+  BleQnsdk,
+  QnsSDKEmitter,
+  buildUser,
+  onStartDiscovery,
+  onStopDiscovery,
+};
 // module.exports = new BleQnsdkManager();
