1f7c1d03f0b46b46840f225392bb34ecd18582cd
Jeffrey Drakos <jeffreydrakos@Jeffreys-MacBook-Pro-2.local>
2024-12-20 14:30:17 -0500

    fix: udpating

--- ios/BleQnsdk.mm
+++ ios/BleQnsdk.mm
@@ -110,7 +110,7 @@ RCT_EXTERN_METHOD(disconnectDevice)
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
-    return std::make_shared<facebook::react::NativeRTNCalculatorSpecJSI>(params);
+    return std::make_shared<facebook::react::NativeBleQnsdkSpecJSI>(params);
 }
 
 @end
